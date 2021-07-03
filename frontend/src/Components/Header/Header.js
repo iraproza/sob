3077ebe10e1e864545b614f33f3bd82a2cba82cf
Iraproza <kolomiira@gmail.com>
2021-07-03 11:53:34 +0300

    add slider

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "../../index.css"
 import {Nav, Navbar, NavDropdown, Container, Form,Button, FormControl} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import "./Header.css";
 
 
 const Header = () => {
@@ -9,23 +10,16 @@ const Header = () => {
         <header className="bg-primary">
             <Container>
                 <Navbar className="navbar navbar-expand-lg navbar-dark">
-                    <Navbar.Brand className="navbar-brand">WOS</Navbar.Brand>
+                    <Navbar.Brand className="navbar-brand"> <i class="far fa-star"></i>SOB</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
                             {/* <Link className="nav-link" to ='/'>Home</Link> */}
-                            <Link className="nav-link" to ='/'>Content</Link>
+                            <Nav.Link className="nav-link" href='#product'>Product</Nav.Link>
                             <Link className="nav-link" to="/about-us">About us</Link>
                             <Link className="nav-link" to="/contact">Contact</Link>
-                            <Nav.Link className="nav-link">Sign In</Nav.Link>
-                            <Nav.Link className="nav-link">Cart </Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
+                            <Link className="nav-link" to={`/product-add`}>Add Product </Link>
+                            
                         </Nav>
                         <Form inline>
                             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
--- frontend/src/Components/Products/Products.js
+++ frontend/src/Components/Products/Products.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import {Button } from 'react-bootstrap';
+import {Button, Carousel } from 'react-bootstrap';
 import "./Products.css";
 import ProductItem from "./ProductItem/ProductItem";
 import { getAllProducts } from "../../Services/api-service";
@@ -24,11 +24,43 @@ import { getAllProd } from "../../Actions/shopActions";
 
         return(
             <Fragment>
+                 <Carousel>
+                    <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="https://images.squarespace-cdn.com/content/v1/5b96faeaa9e0285146f28410/1591054924640-0GZ6N5OI7QARFLX7YTB1/ke17ZwdGBToddI8pDm48kFyD7pzB8zoMIVY5aiUuFlp7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0jG2lbcDYBOeMi4OFSYem8DMb5PTLoEDdB05UqhYu-xbnSznFxIRsaAU-3g5IaylIg/skincare+banner.jpg?format=2500w"
+                        alt="Second slide"
+                        />
+
+                        <Carousel.Caption>
+                        <h3>Discover the Secrets of Nature</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item><img
+                        className="d-block w-100"
+                        src="https://maincream.com/upload/e6c2fd0c-06195742-story-image-20290-cover-1500x900.jpg"
+                        alt="First slide"
+                        />
+                        <Carousel.Caption>
+                        <h3>Essential Guide to Skincare Treatment</h3>
+                       
+                        </Carousel.Caption>
+                       
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://bonvivant.ua/image/catalog/blog/natural-cosmetic.jpg"
+                        alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                        <h3>Wearable Solutions</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
                 <div className="container">
-                    <div className="row products-container">
-                    <Link to={`/product-add`}>
-                        <Button variant="outline-secondary">Add Product</Button>
-                    </Link>
+                    <div className="row products-container" id='product'>
                         {item.length > 0 ? item: <h2>List is empty</h2>} 
                     </div>     
                 </div>   
